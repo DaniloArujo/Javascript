5ad4a1096a78064d88afea67feeed5f9b2c71bee
Danilo <daniloaraujomota@gmail.com>
2022-11-10 02:18:12 -0300

    10/11

---
+++ 003_particularidades/js/scripts.js
@@ -0,0 +1,21 @@
+console.log('Primeiro');
+
+console.log('ultimo');
+
+var a = 1;
+
+a = 'Teste';
+
+console.log(a)
+
+//Este é um comentário
+
+/*
+este
+é
+outro
+comentário
+*/
+
+
+// os comentários no JS são visíveis no front
---
+++ 004_variaveis/js/scripts.js
@@ -0,0 +1,18 @@
+var teste = 1;
+
+var nome = "João"
+
+
+console.log(teste);
+
+teste = 'Danilo';
+
+console.log(teste)
+
+console.log(nome)
+
+var $nome = "asd"
+var _nome = "asd"
+
+console.log($nome)
+console.log(_nome)
