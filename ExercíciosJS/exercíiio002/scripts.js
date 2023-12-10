ed35520870fc212a6e4e173840b758ddc9d66f6e
Danilo <daniloaraujomota@gmail.com>
2023-12-10 10:59:57 -0300

    .

---
+++ ExercíciosJS/exercíiio002/scripts.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+function variaveis(){
+    var sobrenome = "Araujo"
+    if(true){
+        console.log(sobrenome)
+    }
+    console.log(sobrenome)
+}
+variaveis()
+
+console.log(sobrenome)
