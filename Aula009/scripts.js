b94280a0975e0a3ac3a8ee483526b2ec81a9b297
Danilo <daniloaraujomota@gmail.com>
2023-12-26 12:15:16 -0300

    .

---
+++ Aula009/scripts.js
@@ -0,0 +1,38 @@
+
+let userName = "           Danilo Araujo           "
+
+console.log(`userName = ${userName}`)
+
+userName = userName.trim()
+
+console.log(userName)
+
+
+userName = userName.toUpperCase()
+
+console.log(userName)
+
+userName = userName.toLocaleLowerCase()
+
+console.log(userName)
+
+console.log(`Username lenght = ${userName.length}`)
+
+
+console.log(`Username charAt(0) = ${userName.charAt(0)}`)
+
+console.log(`Username charAt(1) = ${userName.charAt(1)}`)
+
+console.log(`Username charAt(2) = ${userName.charAt(2)}`)
+
+console.log(`Username charAt(3) = ${userName.charAt(3)}`)
+
+//nos dá o indice da primeira ocorrência do caracter:
+
+console.log(`Username.indexOf("n") = ${userName.indexOf("n")}`)
+
+console.log(`Username.lastIndexOf("a") = ${userName.lastIndexOf("a")}`)
+
+let phoneNumber = "123"
+
+console.log(`phoneNumber = ${phoneNumber}`)
