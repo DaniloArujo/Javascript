5fb83b69a35d9d6b4fc9f77cae6683cf894bba25
Danilo <daniloaraujomota@gmail.com>
2023-12-29 10:36:39 -0300

    .

---
+++ Aula010/scripts.js
@@ -0,0 +1,8 @@
+
+document.getElementById("isSelected").onclick = ()=>{
+    if(document.getElementById("caixinha").checked == true){
+        document.getElementById("resultado").innerHTML = "checked"
+    }else{
+        document.getElementById("resultado").innerHTML = "not checked"
+    }
+}
