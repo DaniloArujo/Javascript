fcb2df35e6e44545a9e8a80402d4b7158c4ae3c8
Danilo <daniloaraujomota@gmail.com>
2024-01-29 22:32:28 -0300

    .

--- Aula007/scripts.js
+++ Aula007/scripts.js
@@ -4,14 +4,15 @@
 let tempo 
 let sunny 
 
-document.getElementById("enviar").onclick = () =>{
+document.getElementById("enviar").onclick = () => {
     tempo = document.getElementById("temperatura").value;
     tempo = Number(tempo)
 
-    sunny = document.getElementById("sol").
+    sunny = document.getElementById("sol")
     if(tempo > 0 && tempo < 30 && sunny){
-        console.log("O clima está agradável");
+        document.getElementById('resultado').innerHTML = "O clima está agradável"
     }else{
-        console.log("o clima está ruim");
+        
+        document.getElementById('resultado').innerHTML = "O clima está ruim"
     }
 }
